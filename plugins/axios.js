89f9bf266e16540e31c94d516268d638f6a7f8b5
hhandri26 <handrisaeputra@gmail.com>
2020-06-16 17:15:48 +0700

    axios backend

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
-// export default  axios.create({
-//     baseURL: 'http://103.195.90.232:88/api/',
-//     headers: {'Content-Type':'application/json'}
-//   });
+export default  axios.create({
+  baseURL: 'http://localhost:3030/',
+  headers: {'Content-Type':'application/json'}
+});
 
-  export default  axios.create({
-    baseURL: 'http://153.92.4.44:3030/',
-    headers: {'Content-Type':'application/json','Accept':'application/json'}
-  });
+  // export default  axios.create({
+  //   baseURL: 'http://153.92.4.44:3030/',
+  //   headers: {'Content-Type':'application/json','Accept':'application/json'}
+  // });
 
   // virtuals
   // Virtualonline2019##
