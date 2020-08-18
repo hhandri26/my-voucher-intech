1528bf4cb81169c0d0f9b3e4d4c40dcbdfbc4d01
unknown <handrisaeputra@gmail.com>
2020-08-18 23:56:14 +0700

    APPROVE SELECTED TRANSACTION

--- pages/list_transaksi_all_change.vue
+++ pages/list_transaksi_all_change.vue
@@ -1,6 +1,70 @@
 <template>
 
     <div id="app">
+            <v-card>
+            <v-row>
+              <v-col cols="12" >
+                  <v-row>
+                    <v-col cols="3">
+                        Tanggal
+                    </v-col>
+                    <v-col cols="9">
+                      <date-picker v-model="date" range format="YYYY-MM-DD" placeholder="Tanggal"></date-picker>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="3">
+                        Reseller
+                    </v-col>
+                    <v-col cols="4">
+                        <v-autocomplete
+                            v-model="username"
+                            :items="user"
+                            color="blue"
+                            hide-no-data
+                            hide-selected
+                            item-text="username"
+                            item-value="id"
+                            label="Pilih Reseller"
+                        ></v-autocomplete>
+                    </v-col>
+                  </v-row>
+                   
+                   <v-row>
+                    <v-col cols="3">
+                        Status Transaksi
+                    </v-col>
+                    <v-col cols="4">
+                        <v-autocomplete
+                            v-model="stts"
+                            :items="status2"
+                            color="blue"
+                            hide-no-data
+                            hide-selected
+                            item-text="text"
+                            item-value="value"
+                            label="Pilih Status"
+                        ></v-autocomplete>
+                    </v-col>
+                  </v-row>
+                  
+                  <v-row>
+                    <v-col cols="3">
+                         <v-btn depressed small color="primary" @click="do_search()">Cari</v-btn>
+                          <v-btn depressed small color="primary" @click="export_excel()">Export</v-btn>
+                    </v-col>
+                  </v-row>
+                 
+                  <v-row>
+                    <v-col cols="12">
+                         <v-btn depressed small color="primary" @click="selected_change()">Approve transaction</v-btn>
+                        
+                    </v-col>
+                  </v-row>
+              </v-col>
+            </v-row>
+           
+          </v-card>
     <v-snackbar
       v-model="snackbar"
       :multi-line="multiLine"
@@ -55,6 +119,11 @@
             :headers="headers"
             :items="data"
             :search="search"
+             v-model="selected"
+               :single-select="singleSelect"
+              item-key="id"
+              show-select
+              class="elevation-1"
           >
            <template v-slot:top>
                 <v-toolbar flat color="white">
@@ -134,13 +203,17 @@
 import {mapState} from 'vuex'
 import axios from '~/plugins/axios'
 import VueNumeric from 'vue-numeric'
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
 
 export default {
    middleware: 'auth',
    async asyncData({store, error}) {   
     let data       = await axios.get('payment_finance');
+     let user       = await axios.get('users');
       return {
         data:data.data.values,
+         user:user.data.values,
       }
        
     },
@@ -148,6 +221,17 @@ export default {
     
     data: function(){
         return {
+          stts:'',
+          selected:[],
+           status2:[
+                 {text:'Approved', value:'APPROVED'},
+                 {text:'Reject', value:'REJECT'},
+                 {text:'Pre Order', value:'PO'},
+                 {text:'Paid', value:'PAID'},
+             ],
+          date:'',
+          zona:'',
+          username:'',
            dialog2:false,
           persen:false,
             dialog: false,
@@ -189,6 +273,7 @@ export default {
               sortable: false,
               value: 'nomor_transaction',
             },
+             { text: 'Tanggal', value: 'date' },
              { text: 'Reseller', value: 'username' },
             { text: 'Jumlah', value: 'qty' },
              { text: 'Sub Total', value: 'sub_total' },
@@ -229,6 +314,104 @@ export default {
     created(){
     },
     methods: {
+      selected_change(){
+        if(this.selected.length > 0){
+          Object.entries(this.selected).forEach(([key, val]) => {
+             var item = {
+                                id:val.id,
+                              bukti_transfer:val.bukti_transfer,
+                              qty:val.qty,
+                              nomor_transaction:val.nomor_transaction,
+                              id_user:val.id_user,
+                              price:val.price,
+                              status:'PAID'
+
+                            };
+            this.$store.dispatch('approved', {item}).then((res) => {
+                
+                if(res == 200){
+                   this.notif_color ='blue';
+                  this.notif_text ='Berhasil Approve Transaksi ' + val.nomor_transaction;
+                  this.snackbar = true;
+                  
+                   
+                 
+                }
+            })
+         
+
+          });
+          axios.get('payment_finance')
+            .then(res => {
+                  this.data   = res.data.values;               
+                
+            }).catch(err => {
+            console.log(err);
+            })
+
+        }else{
+            this.notif_color ='red';
+                  this.notif_text ='Pilih transaksi terlebidahulu !';
+                  this.snackbar = true;
+
+        }
+
+      },
+      do_search(){
+         this.loading = true;
+        var date1 = null;
+        var date2 = null;
+        if(this.date!==null){
+          date1 = this.$moment(this.date[0]).format("YYYY-MM-DD");
+          date2 =this.$moment(this.date[1]).format("YYYY-MM-DD");
+
+        }
+          
+          axios.post('transaction_report',{date1:date1,date2:date2,username:this.username,zona:this.zona,status:this.stts})
+            .then(res => {
+                  this.data   = res.data.values; 
+                  this.loading = false;              
+                
+            }).catch(err => {
+            console.log(err);
+            })
+
+      },
+      export_excel(){
+          var excel = $JExcel.new();
+          excel.set({ sheet: 0, value: "Report" });
+          var evenRow = excel.addStyle({ border: "none,none,none,thin #333333" });
+          var oddRow = excel.addStyle({ fill: "#ECECEC", border: "none,none,none,thin #333333" });
+
+          var headers = ["Nomor Transaksi", "Reseller", "Jumlah", "Sub Total","Tanggal","Status","lokasi","approved by"];
+
+          for (var i = 0; i < headers.length; i++) {                       
+            // Loop headers
+            excel.set(0, i, 0, headers[i]);
+            // Set CELL header text & header format
+            excel.set(0, i, undefined, "auto");
+          }
+          var i = 1;
+
+        
+          // Set COLUMN width to auto 
+          _.each(this.data, function (value) {
+            
+              excel.set(0, 0, i, value.nomor_transaction);
+              excel.set(0, 1, i, value.username);
+              excel.set(0, 2, i, value.qty);
+              excel.set(0, 3, i, value.harga);
+              excel.set(0, 4, i, value.date);
+              excel.set(0, 5, i, value.status);
+              excel.set(0, 6, i, value.lok);
+              excel.set(0, 7, i, value.approved_by);
+              
+              i = i + 1;
+          });
+         
+
+          excel.generate("report_Transaksi_" + Date.now() + ".xlsx");
+        },
         show(item){
 
        this.dialog2 = true;  
@@ -303,6 +486,7 @@ export default {
     },
     components: {
       VueNumeric,
+      DatePicker
       
     }
 }
